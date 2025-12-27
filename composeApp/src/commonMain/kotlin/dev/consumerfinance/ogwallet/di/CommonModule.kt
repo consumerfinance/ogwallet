26cd484a125c7a8b60658ba4efbd9680a3f90259
techlord991 <lordmagic@magicians.ggg>
2025-12-27 15:01:51 +0530

    sms

--- composeApp/src/commonMain/kotlin/dev/consumerfinance/ogwallet/di/CommonModule.kt
+++ composeApp/src/commonMain/kotlin/dev/consumerfinance/ogwallet/di/CommonModule.kt
@@ -1,6 +1,7 @@
 package dev.consumerfinance.ogwallet.di
 
 import dev.consumerfinance.ogwallet.db.BillRepository
+import dev.consumerfinance.ogwallet.db.CreditCardRepository
 import dev.consumerfinance.ogwallet.db.DatabaseManager
 import dev.consumerfinance.ogwallet.db.TransactionRepository
 import org.koin.dsl.module
@@ -12,6 +13,7 @@ val commonModule = module {
     single { DatabaseManager(get()) }
     single { TransactionRepository(get()) }
     single { BillRepository(get()) }
+    single { CreditCardRepository(get()) }
     single { DataExportService(get(), get()) } // Inject TransactionRepository and BillRepository
     single { SmsParser(get()) }
 }
